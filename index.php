b5a65f8b5b6ef00c6687fb93b6760a41e5bd5327
volosgoto <volosgoto@yahoo.com>
2018-05-07 11:23:07 +0300

    upd add comments

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
+// Подключаем автозагрузчик классов autoloader.php
 require_once 'autoloader.php';
 use app\Cat;
 
-
 $cat = new Cat('garfield');
-echo $cat->getName();
+
+// Выводит имя кота
+//$cat->getName()
+
+// Выводит имя кота c проверкой
+if ($cat->getName()) {
+    echo $cat->getName();
+}
+
+// Выводит имя кота c проверкой через тернарный оператор
+//$catName = $cat->getName() ? $cat->getName() : '';
+//echo $catName;
+
+//Выводит имя кота c проверкой. Работает только на PHP 7.0 или выше
+//echo $catName = $cat->getName() ?? '';
+
 echo '</br>';
+
+// Все проверки по аналогии подставить $cat->meow() вместо $cat->getName()
 echo $cat->meow();
